6bd56b6a3149a9dd17d2f188237e9b47b15607b0
dannielgloria <danniel.gloria@gmail.com>
2025-05-06 21:33:26 -0600

    feat:  Se crean scriops para ejecucion dependiendo de el entorno

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,11 @@
-import dotenv from 'dotenv'
+import dotenv, { config } from 'dotenv';
+import path from 'path';
+import { env } from 'process';
 
-dotenv.config();
+const envPath = path.resolve(process.cwd(),`.env.${process.env.NODE_ENV || 'development'}`)
+console.log("Environment:", process.env.NODE_ENV);
+
+dotenv.config({ path: envPath});
 
 export const port = process.env.PORT_APP
 export const uri = process.env.URI_APP
