aeef2d39847c4966c06af247a08b1a3b63f9baa8
dannielgloria <danniel.gloria@gmail.com>
2025-05-08 20:55:46 -0600

    feat:  Se crean la logica para crear Post a partir de unvinculo con el User y mostrar todos los Post

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { getAllUsers, createUser } from '../controllers/userController.js';
+import { createPost, getAllPosts } from '../controllers/postController.js';
 
 const router = express.Router();
-
+http://localhost:5001/api/v0/users
 router.route('/').get(getAllUsers).post(createUser);
+
+http://localhost:5001/api/v0/users/posts
+router.route('/posts').get(getAllPosts).post(createPost);
 
 export default router;
