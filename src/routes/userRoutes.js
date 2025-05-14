3f06766670a4922295317a0af441c7ba399d5922
dannielgloria <danniel.gloria@gmail.com>
2025-05-13 20:23:45 -0600

    feat:  Se agrega middleware para la autorizacion de uso de enpoins

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getAllUsers, createUser } from '../controllers/userController.js';
 import { createPost, getAllPosts } from '../controllers/postController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 //http://localhost:5001/api/v0/users
-router.route('/').get(getAllUsers).post(createUser);
+router.route('/').get(protect, getAllUsers).post(createUser);
 
 //http://localhost:5001/api/v0/users/posts
-router.route('/posts').get(getAllPosts).post(createPost);
+router.route('/posts').get(protect, getAllPosts).post(createPost);
 
 export default router;
