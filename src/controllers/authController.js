1b6dea87b6d9cc1a8a4de6c7c2e0103fa34468f7
dannielgloria <danniel.gloria@gmail.com>
2025-05-13 19:26:33 -0600

    feat:  Se agrega la logica para el login en aplicacion

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/Users.js";
 import {keyToken} from "../config/constants.js"
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const generateToken = (id) =>{
     return jwt.sign({id},keyToken, {expiresIn: '30d'});
@@ -32,3 +33,21 @@ export const registerUser = async (req,res) => {
         res.status(400).json({message: 'Invalid user data'});
     }
 };
+
+// Logica (2) nos logeamos a nuestra app
+export const authenticateUser = async (req,res) =>{
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    console.log("USUARIO: "+user)
+
+    if (user && (await bcrypt.compare(password, user.password) )) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400).json({message: 'Invalid email or password'});
+    }
+}
