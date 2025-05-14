76bdc2d311f4c10958b349d5ea8b35b0870f70e0
dannielgloria <danniel.gloria@gmail.com>
2025-05-13 20:59:10 -0600

    feat:  Se agrega middleware para validacion y manejo de errores

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { port, uri } from './src/config/constants.js';
 import healtCheckRoutes from './src/routes/healtCheckRoutes.js'
 import userRoutes from './src/routes/userRoutes.js'
 import authRoutes from './src/routes/authRoutes.js'
+import { generalErrorHandler, celebrateErrorHandler } from './src/middleware/errorMiddleware.js';
 
 connectDB();
 
@@ -18,6 +19,10 @@ app.use(`${uri}/users`, userRoutes);
 
 //http://localhost:5001/api/v0/auth
 app.use(`${uri}/auth`, authRoutes);
+
+// Se agregan manejo de errores de celebrate
+app.use(celebrateErrorHandler);
+app.use(generalErrorHandler);
 
 const PORT = port || 5000;
 app.listen(PORT, console.log(`Server running on http://localhost:${PORT}${uri}`));
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 import { getAllUsers, createUser } from '../controllers/userController.js';
 import { createPost, getAllPosts } from '../controllers/postController.js';
 import { protect } from '../middleware/authMiddleware.js';
+import { userValidation } from '../middleware/userValidationMiddleware.js';
 
 const router = express.Router();
 //http://localhost:5001/api/v0/users
-router.route('/').get(protect, getAllUsers).post(createUser);
+router.route('/').get(protect, getAllUsers).post(userValidation,createUser);
 
 //http://localhost:5001/api/v0/users/posts
 router.route('/posts').get(protect, getAllPosts).post(createPost);
