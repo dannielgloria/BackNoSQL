7168f6e8da6eff4241e27842f944c5fb760bc770
dannielgloria <danniel.gloria@gmail.com>
2025-05-21 19:48:58 -0600

    feat:  Se actualizan prueba unitaria de contorlador
    --TODO: alumnis deben de terminar las pruebas unitarias y agregar pruebas unitarias de almenos un controllador distinto

--- test/integration/authController.test.js
+++ test/integration/authController.test.js
@@ -40,20 +40,34 @@ describe('Integracion de Test del controlador authController.js', () => {
         expect(response.body.name).toBe('Pilar Mondragon Hernandez');
     });
 
-    test('Registro de Usuario Exitoso', async () => { 
+    test('Registro de Usuario Erroneo (usuario ya existente)', async () => {
+        await User.create({
+            "name": "Hugo Franco Gutiérrez",
+            "email": "hfg@gmail.com",
+            "password": "124rq3rcq"
+        });
+
         const response = await resquest(app)
             .post('/api/v1/auth/register')
             .send({
-                "name": "Pilar Mondragon Hernandez",
-                "email": "pmh@gmail.com",
-                "password": "aguacate"
+                "name": "Hugo Franco Gutiérrez",
+                "email": "hfg@gmail.com",
+                "password": "1234"
             });
         
-        expect(response.statusCode).toBe(201);
-        expect(response.body).toHaveProperty('_id');
-        expect(response.body).toHaveProperty('tokenAcces');
-        expect(response.body.email).toBe('pmh@gmail.com');
-        expect(response.body.name).toBe('Pilar Mondragon Hernandez');
+        expect(response.statusCode).toBe(400);
+        expect(response.body.message).toBe('User already exists');
     });
 
+    test('Registro de Usuario Erroneo (Faltan datos/ datos erroneos)', async () => {
+        const response = await resquest(app)
+            .post('/api/v1/auth/register')
+            .send({
+                "name": "Hugo Franco Gutiérrez",
+                "password": "1234"
+            });
+        
+        expect(response.statusCode).toBe(500);
+        expect(response.body.message).toBe('Internal Server Error');
+    });
 });
